266cdb0cb3a7f432c8544b57a11eb62ec5338d29
Oliver Clarke <olibclarke@gmail.com>
2018-02-12 12:28:26 -0500

    Update star2jpg.bash

--- star2jpg.bash
+++ star2jpg.bash
@@ -6,47 +6,43 @@ RADIUS=175 #particle radius in pixels
 MIC_FIELD=`grep _rlnMicrographName "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
 X_FIELD=`grep _rlnCoordinateX "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
 Y_FIELD=`grep _rlnCoordinateY "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
-PMAX_FIELD=`grep _rlnMaxValueProbDistribution "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
-NSS_FIELD=`grep _rlnNrOfSignificantSamples "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
 DEF_FIELD=`grep _rlnDefocusU "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
-PART_FIED=`grep _rlnImageName "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
+PART_FIELD=`grep _rlnImageName "$@" | awk 'BEGIN {FS="#"} ; {print $2}'`
 
 awk -v mic_field=$MIC_FIELD '(NF>=3){print $mic_field}' "$@" | sort | uniq > tmp_mic_list
 sed -i '/^$/d' tmp_mic_list
 
+#check that we have Imagemagick etc:
+hash convert 2>/dev/null || { echo >&2 "Can't find convert - check that ImageMagick is available"; exit 1; }
+
+
 mkdir jpg_out
 
 while read line; do
-  mic_name=$(cut -d ' ' -f$MIC_FIELD <<< $line)
+  mic_name=$(cut -d ' ' -f${MIC_FIELD} <<< $line)
   mrc2tif -S -C ${mic_name} tmp.tif
   mic_name=${mic_name##*/}
-  convert -contrast-stretch -4%x0.5% -auto-gamma -brightness-contrast 10x60 -resize 20% -rotate 180 -flop tmp.tif ${mic_name%.mrc}.jpg
-  cp ${mic_name%.mrc}.jpg ${mic_name%.mrc}_orig.jpg
   grep $mic_name "$@" > single_mic.star
   particle_count=`cat single_mic.star | wc -l`
-  pmax_sum=0.0
-  nss_sum=0
+  convert tmp.tif -morphology Convolve Gaussian:0x2 tmp.tif
+  convert -contrast-stretch -4%x0.5% -auto-gamma -brightness-contrast 10x20 -resize 15% -rotate 180 -flop tmp.tif ${particle_count}p_${mic_name%.mrc}.jpg
+  cp ${particle_count}p_${mic_name%.mrc}.jpg tmp_unlabeled.jpg
   while read line2; do
-   x_coord=$(cut -d ' ' -f$X_FIELD <<< $line2)
-   y_coord=$(cut -d ' ' -f$Y_FIELD <<< $line2)
-   pmax=$(cut -d ' ' -f$PMAX_FIELD <<< $line2)
-   defocus=$(cut -d ' ' -f$DEF_FIELD <<< $line2)
-   x_coord=`echo "$x_coord*0.2" | bc -l`
-   y_coord=`echo "$y_coord*0.2" | bc -l`
-   part_name=$(cut -d ' ' -f$PART_FIED <<< $line2)
+   x_coord=$(cut -d ' ' -f${X_FIELD} <<< $line2)
+   y_coord=$(cut -d ' ' -f${Y_FIELD} <<< $line2)
+   defocus=$(cut -d ' ' -f${DEF_FIELD} <<< $line2)
+   x_coord=`echo "$x_coord*0.15" | bc -l`
+   y_coord=`echo "$y_coord*0.15" | bc -l`
+   part_name=$(cut -d ' ' -f${PART_FIELD} <<< $line2)
    part_no=$(cut -d '@' -f1 <<< $part_name)
    part_no=$((10#$part_no))
-   edge=`echo "$x_coord+($RADIUS*0.2)" | bc -l`
-   convert -stroke Firebrick -strokewidth 0.5 -fill none -draw "circle ${x_coord},${y_coord} ${edge},${y_coord}" -fill Firebrick -annotate +$edge+$y_coord "${part_no}" ${mic_name%.mrc}.jpg ${mic_name%.mrc}.jpg
+   edge=`echo "$x_coord+($RADIUS*0.15)" | bc -l`
+   convert -stroke Firebrick -strokewidth 1 -fill none -draw "circle ${x_coord},${y_coord} ${edge},${y_coord}" -fill Firebrick ${particle_count}p_${mic_name%.mrc}.jpg ${particle_count}p_${mic_name%.mrc}.jpg
   done < single_mic.star
-  nss_ave=`awk -v nss_field=$NSS_FIELD '{ sum += $nss_field } END { if (NR > 0) print sum / NR }' single_mic.star`
-  nss_ave=$( printf "%4.1f" $nss_ave )
-  pmax_ave=`awk -v pmax_field=$PMAX_FIELD '{ sum += $pmax_field } END { if (NR > 0) print sum / NR }' single_mic.star`
-  pmax_ave=$( printf "%3.4f" $pmax_ave )
   defocus=`echo "$defocus*0.0001" | bc -l`
   defocus=$( printf "%1.1f" $defocus )
-  convert -gravity northwest -pointsize 20 -annotate 0x0+0+15 "Defocus: ${defocus}µm, Mean Pmax: ${pmax_ave}, Mean NSS: ${nss_ave}, ${particle_count} particles" ${mic_name%.mrc}.jpg ${mic_name%.mrc}.jpg
-  montage -geometry +2+2 -background '#000000' ${mic_name%.mrc}_orig.jpg ${mic_name%.mrc}.jpg ${mic_name%.mrc}.jpg
-  mv ${mic_name%.mrc}.jpg ./jpg_out
-  rm ${mic_name%.mrc}_orig.jpg
+  convert -gravity northwest -fill DarkBlue -pointsize 20 -annotate 0x0+0+15 "Defocus: ${defocus}µm, ${particle_count} particles" ${particle_count}p_${mic_name%.mrc}.jpg ${particle_count}p_${mic_name%.mrc}.jpg
+  convert  ${particle_count}p_${mic_name%.mrc}.jpg tmp_unlabeled.jpg +append ${particle_count}p_${mic_name%.mrc}.jpg
+  mv ${particle_count}p_${mic_name%.mrc}.jpg ./jpg_out
+  rm tmp_unlabeled.jpg
 done < tmp_mic_list
