2f3a8d91a4607685a3d6662882b3a00f22ec42f0
olibclarke <olibclarke@gmail.com>
2019-01-22 15:27:09 -0500

    Update ice_catcher.bash

--- ice_catcher.bash
+++ ice_catcher.bash
@@ -1,8 +1,11 @@
 #! /bin/bash
 #Run on *avrot.txt
 #e.g.: ./ice_catcher.bash *avrot.txt
+#A threshold of 2 seems to catch the worst offenders.
+#Or just run with a threshold of -1 and take a look at the sorted file to figure out where to draw the line.
+rm icy_list.txt
 touch icy_list.txt
-ice_threshold=1.0
+ice_threshold=-1
 for i in "$@"
 do
 basename=${i%_ctf_avrot.txt}
@@ -31,3 +34,4 @@ if (( $(echo "$ice_intensity > $ice_threshold" | bc -l ) )); then
 echo "${basename} ${ice_intensity}" >> icy_list.txt
 fi
 done
+sort -k2nr icy_list.txt > icy_list_sorted.txt
